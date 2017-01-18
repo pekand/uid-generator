4a9ae90699f8554b7dce43b2060d4a4384ecd973
Pekar Andrej <pekand@gmail.com>
2017-01-18 23:07:48 +0100

    init

---
+++ UidApp/UidForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UidApp
+{
+    public partial class UidForm : Form
+    {
+        public UidForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Guid.NewGuid().ToString("N").Substring(0, 10);
+            Clipboard.SetText(textBox1.Text);
+        }
+    }
+}
